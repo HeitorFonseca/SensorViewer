288fc78619315328bbae7b544a5b98a7b97bcb09
Heitor Araujo <hfa2@cin.ufpe.br>
2018-02-26 10:18:35 -0300

    Add sqlite dll and png Add button

--- SensorsViewer/obj/Debug/SensorsViewer_Content.g.i.cs
+++ SensorsViewer/obj/Debug/SensorsViewer_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/newprojectimg.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/transpnewprojectimg3.png")]
 
 
